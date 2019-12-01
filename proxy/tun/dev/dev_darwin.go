39ddd92dfc16630ab21bd188fda9585d31c9cad9
Comzyh <comzyh@gmail.com>
2019-12-01 19:09:04 +0800

    Fix. Meet netstack(20191123085552-55fcc16cd0eb) packet style.

--- proxy/tun/dev/dev_darwin.go
+++ proxy/tun/dev/dev_darwin.go
@@ -237,7 +237,9 @@ func (t *tunDarwin) AsLinkEndpoint() (result stack.LinkEndpoint, err error) {
 			case header.IPv6Version:
 				p = header.IPv6ProtocolNumber
 			}
-			linkEP.Inject(p, buffer.View(packet[:n]).ToVectorisedView())
+			linkEP.InjectInbound(p, tcpip.PacketBuffer{
+				Data: buffer.View(packet[:n]).ToVectorisedView(),
+			})
 		}
 		t.wg.Done()
 		t.Close()
@@ -255,7 +257,9 @@ func (t *tunDarwin) AsLinkEndpoint() (result stack.LinkEndpoint, err error) {
 			case <-t.stopW:
 				break packetLoop
 			}
-			_, err := t.Write(buffer.NewVectorisedView(len(packet.Header)+len(packet.Payload), []buffer.View{packet.Header, packet.Payload}).ToView())
+			header := packet.Pkt.Header.View()
+			data := packet.Pkt.Data.ToView()
+			_, err := t.Write(buffer.NewVectorisedView(len(header)+len(data), []buffer.View{header, data}).ToView())
 			if err != nil {
 				log.Errorln("Can not read from tun: %v", err)
 				break

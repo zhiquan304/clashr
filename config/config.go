7c2f3e6bc5662a6555931a958806843d3574b704
Comzyh <comzyh@gmail.com>
2019-12-17 00:17:40 +0800

    DNS server on tun

--- config/config.go
+++ config/config.go
@@ -60,6 +60,7 @@ type FallbackFilter struct {
 type Tun struct {
 	Enable    bool   `yaml:"enable" json:"enable"`
 	DeviceURL string `yaml:"device-url" json:"device-url"`
+	DNSListen string `yaml:"dns-listen" json:"dns-listen"`
 }
 
 // Experimental config
@@ -138,6 +139,7 @@ func Parse(buf []byte) (*Config, error) {
 		Tun: Tun{
 			Enable:    false,
 			DeviceURL: "dev://clash0",
+			DNSListen: "",
 		},
 		Experimental: Experimental{
 			IgnoreResolveFail: true,
